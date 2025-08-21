91173ff7ef2e77eb769680a298f19886f5321674
Enzo-Palavicino <enzo.palavicino@gmail.com>
2025-08-21 14:28:36 -0400

    Entrega

---
+++ app/models/about.rb
@@ -0,0 +1,6 @@
+class About < ApplicationRecord
+  # This model is currently empty, but you can add validations, associations, or methods as needed.
+  # For example:
+  # validates :title, presence: true
+  # has_many :comments
+end
